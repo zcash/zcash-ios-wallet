104e44201957969e4ee342b5923734c490c8f3b0
Francisco Gindre <francisco.gindre@gmail.com>
2019-12-30 20:17:00 -0300

    Scaffold Seed Backup screen

--- wallet/wallet/Views/BackupWallet.swift
+++ wallet/wallet/Views/BackupWallet.swift
@@ -17,6 +17,7 @@ struct BackupWallet: View {
     let itemSpacing: CGFloat = 24
     let buttonPadding: CGFloat = 40
     let buttonHeight: CGFloat = 58
+     @State private var showModal: Bool = false
     var body: some View {
         
         ZStack {
@@ -29,11 +30,14 @@ struct BackupWallet: View {
                 Spacer()
                 Text("92% Synced")
                 Spacer()
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {
+                     self.showModal = true
+                }) {
                     ZcashButton(color: Color.black, fill: Color.zYellow, text: "Backup Wallet")
                         .frame(height: buttonHeight)
                 }
                 .padding([.leading, .trailing], buttonPadding)
+                
                 
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Restore")
@@ -47,11 +51,26 @@ struct BackupWallet: View {
         }
     .navigationBarTitle(Text(""))
     .navigationBarHidden(true)
+    .sheet(isPresented: self.$showModal) {
+        SeedBackup(words: FakeProvider().seedWords(limit: 16))
+    }
     }
 }
 
 struct BackupWallet_Previews: PreviewProvider {
     static var previews: some View {
         BackupWallet()
+    }
+}
+
+
+protocol SeedWordsProvider {
+    func seedWords(limit: Int) -> [String]
+}
+
+struct FakeProvider: SeedWordsProvider {}
+extension SeedWordsProvider {
+    func seedWords(limit: Int) -> [String] {
+        ["volume", "burst", "illegal", "swap", "neck", "brother", "foil", "gain", "thought", "glass", "unfold", "mercy", "kangaroo", "faculty", "divorce"]
     }
 }
---
+++ wallet/wallet/Views/SeedBackup.swift
@@ -0,0 +1,62 @@
+//
+//  SeedBackup.swift
+//  wallet
+//
+//  Created by Francisco Gindre on 12/30/19.
+//  Copyright © 2019 Francisco Gindre. All rights reserved.
+//
+
+import SwiftUI
+
+struct SeedBackup: View {
+    var words: [String]
+    
+    let wordsPerRow = 4
+    
+    var body: some View {
+        ZStack {
+            Color.black
+            VStack(alignment: .leading, spacing: 24) {
+                Text("Your Seed Backup")
+                    .font(.title)
+                    .foregroundColor(Color.zYellow)
+               
+                
+                    VStack(alignment: .center, spacing: 8) {
+                        
+                        ForEach(words.chunked(into: wordsPerRow), id: \.self) { row in
+                            
+                            HStack(alignment: .firstTextBaseline, spacing: 20) {
+                                ForEach(row, id:\.self) { word in
+                                    Text(word)
+                                        .foregroundColor(Color.zYellow)
+                                        .font(.headline)
+                                        .lineLimit(1)
+                                        .padding(8)
+                                        .background(Color.zGray)
+                                    
+                                }
+                            }
+                        }
+                    }
+                
+            }
+        }
+        .navigationBarTitle("Your Seed Backup")
+    }
+}
+
+struct SeedBackup_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        return SeedBackup(words: FakeProvider().seedWords(limit: 16))
+    }
+}
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
