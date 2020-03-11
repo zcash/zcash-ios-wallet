893c27af32add0bb2fda2b3e803152d9f0153482
Francisco Gindre <francisco.gindre@gmail.com>
2020-03-10 22:45:54 -0300

    Added Crashlytics

--- wallet/wallet/AppDelegate.swift
+++ wallet/wallet/AppDelegate.swift
@@ -13,13 +13,15 @@ var logger = SimpleLogger(logLevel: .debug, type: SimpleLogger.LoggerType.printe
 #else
 var logger = SimpleLogger(logLevel: .debug)
 #endif
+
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {  
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         let environment = ZECCWalletEnvironment.shared
-        
         switch environment.state {
         case .initalized,
              .synced:
