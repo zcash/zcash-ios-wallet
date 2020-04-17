36395e2867d06364f541fc69569fb9acea08f750
Francisco Gindre <francisco.gindre@gmail.com>
2020-04-17 16:29:56 -0300

    improvements on z address validation on mode

--- wallet/wallet/Utils/String+Zcash.swift
+++ wallet/wallet/Utils/String+Zcash.swift
@@ -10,14 +10,15 @@ import Foundation
 
 
 extension String {
+    
+    var isValidShieldedAddress: Bool {
+           ZECCWalletEnvironment.shared.initializer.isValidShieldedAddress(self)
+       }
+    
     var shortZaddress: String? {
-        guard isValidZaddress else { return nil }
+        guard isValidShieldedAddress else { return nil }
         return String(self[self.startIndex ..< self.index(self.startIndex, offsetBy: 8)])
             + "..."
             + String(self[self.index(self.endIndex, offsetBy: -8) ..< self.endIndex])
-    }
-    
-    var isValidZaddress: Bool {
-        return count > 16 && (self.starts(with: "z") || self.starts(with: "Z"))
     }
 }
