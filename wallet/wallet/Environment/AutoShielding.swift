87db1ed17d64ce6f6f5167a0d34fc3371a05aa21
Francisco Gindre <francisco.gindre@gmail.com>
2021-06-25 20:47:07 -0300

    ThresholdDrivenAutoShielding

--- wallet/wallet/Environment/AutoShielding.swift
+++ wallet/wallet/Environment/AutoShielding.swift
@@ -27,6 +27,13 @@ protocol UserSession {
     func markAutoShield()
 }
 
+typealias PrivateKeyAccountIndexPair = (privateKey: String, account: Int, index: Int)
+
+protocol ShieldingKeyProviding {
+    func getTransparentSecretKey() throws -> PrivateKeyAccountIndexPair
+    func getSpendingKey() throws -> PrivateKeyAccountIndexPair
+}
+
 class Session {
     static var unique = Session()
     
@@ -68,5 +75,57 @@ class ConcreteAutoShielder: AutoShielder {
     init(autoShielding: AutoShieldingStrategy) {
         self.strategy = autoShielding
     }
+}
+
+class ThresholdDrivenAutoShielding: AutoShieldingStrategy {
     
+    var shouldAutoShield: Bool {
+        // Shields after first sync, once per session.
+        session.didFirstSync && !session.alreadyAutoShielded
+    }
+    
+    var synchronizer: Synchronizer
+    var session: UserSession
+    var keyProviding: ShieldingKeyProviding
+    var threshold: Int64
+
+    init(session: UserSession,
+         synchronizer: Synchronizer,
+         keyProviding: ShieldingKeyProviding,
+         threshold zatoshiThreshold: Int64) {
+        self.session = session
+        self.synchronizer = synchronizer
+        self.keyProviding = keyProviding
+        self.threshold = zatoshiThreshold
+    }
+    
+    func shield() -> Future<AutoShieldingResult, Error> {
+        Future<AutoShieldingResult, Error> { promise in
+            
+            do {
+                let spendingKeyKeyPair = try self.keyProviding.getSpendingKey()
+                let tskKeyPair = try self.keyProviding.getTransparentSecretKey()
+                let fromAccount = tskKeyPair.account
+                let tsk = tskKeyPair.privateKey
+                let spendingKey = spendingKeyKeyPair.privateKey
+                // TODO: add parameters to vary the index and the account to shield from
+                let tAddress = try DerivationTool.default.deriveTransparentAddressFromPrivateKey(tsk)
+                
+                // this strategy attempts to shield once per session, regardless of the result.
+                self.session.markAutoShield()
+                
+                self.synchronizer.shieldFunds(spendingKey: spendingKey, transparentSecretKey: tsk, memo: "Shielding from your t-address:\(tAddress)", from: fromAccount) { result in
+                    
+                    switch result {
+                    case .success(let pendingTx):
+                        promise(.success(.shielded(pendingTx: pendingTx)))
+                    case .failure(let error):
+                        promise(.failure(error))
+                    }
+                }
+            } catch {
+                promise(.failure(ShieldFundsError.keyDerivationError(underlyingError: error)))
+            }
+        }
+    }
 }
