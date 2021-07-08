b369b217daf07a60ef5453913a22f53f5a9437d9
Francisco Gindre <francisco.gindre@gmail.com>
2021-07-08 18:59:35 -0300

    Add see details and done buttons to autoshielding

--- wallet/wallet/Views/AutoShielding/AutoShieldView.swift
+++ wallet/wallet/Views/AutoShielding/AutoShieldView.swift
@@ -8,15 +8,16 @@
 
 import SwiftUI
 import Combine
+import ZcashLightClientKit
 final class AutoShieldingViewModel: ObservableObject {
     enum State {
         case shielding
         case failed(error: Error)
-        case success
+        case success(shieldingTx: PendingTransactionEntity)
     }
-    
+
     @Published var state: State = .shielding
-    
+    @Published var seeDetailsActive: DetailModel? = nil
     var shielder: AutoShielder
     var cancellables = [AnyCancellable]()
     init(shielder: AutoShielder) {
@@ -28,13 +29,24 @@ final class AutoShieldingViewModel: ObservableObject {
             let shieldFlow = try ShieldFlow.startWithShilderOrFail(self.shielder)
             
                 shieldFlow.status
+                    .filter( {
+                        switch $0 {
+                        case .notNeeded:
+                            return false
+                        default:
+                            return true
+                        }
+                    } )
+            
                 .map { status -> State in
                     switch status {
-                    case .ended:
-                        return State.success
+                    case .ended(let shieldingTx):
+                        return State.success(shieldingTx: shieldingTx)
                     case .notStarted,
                          .shielding:
                         return State.shielding
+                    case .notNeeded:
+                        return State.shielding //this is filtered beforehand
                     }
                 }
                 .receive(on: DispatchQueue.main)
@@ -61,7 +73,7 @@ final class AutoShieldingViewModel: ObservableObject {
 struct AutoShieldView: View {
     
     @EnvironmentObject var viewModel: AutoShieldingViewModel
-    @Binding var isShown: Bool
+    @Binding var isPresented: Bool
     var body: some View {
         ZStack {
             ZcashBackground()
@@ -74,20 +86,32 @@ struct AutoShieldView: View {
         .onDisappear() {
             ModelFlyWeight.shared.dispose(flyweight: viewModel)
         }
+        .sheet(item: self.$viewModel.seeDetailsActive) {
+            self.isPresented = false
+        } content: { model in
+            TxDetailsWrapper(row: model)
+        }
     }
     
     @ViewBuilder func viewFor(_ state: AutoShieldingViewModel.State) -> some View {
         switch state {
         case .shielding:
             Self.shieldingScreen()
-        case .success:
-            success()
+        case .success(let shieldingTx):
+            Self.success(shieldingTx: DetailModel(pendingTransaction: shieldingTx)) {
+                self.viewModel.seeDetailsActive = DetailModel(pendingTransaction: shieldingTx)
+            } dismissBlock: {
+                self.isPresented.toggle()
+            }
         case .failed(let error):
-            failureScreen(error: error)
+            failureScreen(error: error, dismissBlock: {
+                self.isPresented.toggle()
+            })
         }
     }
     
-    @ViewBuilder func failureScreen(error: Error) -> some View {
+    @ViewBuilder func failureScreen(error: Error,
+                                    dismissBlock: @escaping () -> Void) -> some View {
         VStack(alignment: .center, spacing: 40) {
             Image("sadZebra")
             Text("Autoshielding failed with this error:")
@@ -101,12 +125,15 @@ struct AutoShieldView: View {
                 .foregroundColor(.zLightGray2)
                 .font(.body)
             
-            dismissButton(text: Text("Dismiss")
-                            .foregroundColor(.black))
+            Self.dismissButton(text: Text("Dismiss")
+                            .foregroundColor(.black),
+                               action: dismissBlock)
         }
     }
     
-    @ViewBuilder func success() -> some View {
+    @ViewBuilder static func success(shieldingTx: DetailModel,
+                                     seeDetails: @escaping () -> Void,
+                                     dismissBlock: @escaping () -> Void) -> some View {
         VStack(alignment: .center, spacing: 40) {
             VStack(alignment: .center, spacing: 30) {
             Image("profile_yellowzebra")
@@ -121,16 +148,29 @@ struct AutoShieldView: View {
                             animationType: .playOnce)
                 .frame(height: 100)
             
-            
+            Button(action: {
+                seeDetails()
+            }, label: {
+                Text("button_seedetails")
+                    .foregroundColor(.white)
+                    .zcashButtonBackground(
+                        shape:.roundedCorners(fillStyle:
+                                                .outline(color: .white,
+                                                     lineWidth: 2)))
+                    .frame(height: 48, alignment: .center)
+                                            
+            })
             dismissButton(text: Text("Dismiss")
-                                    .foregroundColor(.black))
+                            .foregroundColor(.black), action: dismissBlock)
+                
             
         }
     }
     
-    @ViewBuilder func dismissButton(text: Text) -> some View {
+    @ViewBuilder static func dismissButton(text: Text,
+                                           action: @escaping () -> Void) -> some View {
         Button(action: {
-            self.isShown = false
+            action()
         }, label: {
             text.zcashButtonBackground(
                 shape: .roundedCorners(fillStyle: .solid(color: .zYellow)))
@@ -156,9 +196,12 @@ struct AutoShieldView: View {
     }
 }
 
+//
+//struct AutoShield_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AutoShieldView()
+//    }
+//}
 
-struct AutoShield_Previews: PreviewProvider {
-    static var previews: some View {
-        AutoShieldView(isShown: .constant(true))
-    }
-}
+
+
